e40eb50f1ab5d6ec4ff893197cdb495a16ca84b9
vipyeshuai <836246914@qq.com>
2013-11-15 15:42:59 +0800

    yeshuai

---
+++ Operator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HSMS.Model
+{
+   public class Operator
+    {
+       public Guid Id { set; get; }
+       public string UserName { set; get; }
+       public string Password { set; get; }
+       public bool IsDelete { set; get; }
+       public string RealName { set; get; }
+       public bool IsNocked { set; get; }
+    }
+}
