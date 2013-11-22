c47b9084b288de60e437aca9d7bd702227f91a90
vipyeshuai <836246914@qq.com>
2013-11-22 11:59:27 +0800

    yeshuai

---
+++ SalarySheet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HSMS.Model
+{
+  public  class SalarySheet
+    {
+      public Guid Id { set; get; }
+      public int Year { set; get; }
+      public int Month { set; get; }
+      public Guid DeptId { set; get; }
+    }
+}
---
+++ SalarySheetItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HSMS.Model
+{
+  public  class SalarySheetItem
+    {
+      public Guid Id { set; get; }
+      public Guid SheetId { set; get; }
+      public Guid EmployeeId { set; get; }
+      public decimal BaseSalary { set; get; }
+      public decimal Bonus { set; get; }
+      public decimal Fine { set; get; }
+      public decimal Other { set; get; }
+    }
+}
