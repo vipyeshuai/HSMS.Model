93da04b3bbba63276d3a6b764dbaf74ffa9313b6
orjlove <303650201@qq.com>
2013-12-08 22:40:25 +0800

    orjlove

---
+++ IdName.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HSMS.Model
+{
+    public class IdName
+    {
+        public Guid Id { set; get; }
+        public string Name { set; get; }
+    }
+}
