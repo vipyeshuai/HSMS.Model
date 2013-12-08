ab6406d2a8c7c394887a626b698e4fe0942b8b63
orjlove <303650201@qq.com>
2013-12-08 22:38:38 +0800

    orjlove

---
+++ Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HSMS.Model
+{
+   public class Department
+    {
+        public Guid Id { set; get; }
+        public string Name { set; get; }
+        public bool IsStopped { set; get; }
+    }
+}
