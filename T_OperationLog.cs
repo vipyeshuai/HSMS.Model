10c97175a1a3f1ba8235f8e3914a3cf917607512
orjlove <303650201@qq.com>
2013-12-08 22:41:15 +0800

    orjlove

---
+++ T_OperationLog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HSMS.Model
+{
+    public class T_OperationLog
+    {
+        public System.Guid Id { set; get; }
+        public System.Guid OperatorId { set; get; }
+        public System.DateTime MakeDate { set; get; }
+        public System.String ActionDesc { set; get; }
+    }
+}
